564414af29ed45746c7129f57fdfdc293554098c
Muh Roshan <74614943+Roshan-Here@users.noreply.github.com>
2022-12-06 23:32:21 +0530

    qr~

---
+++ Inter/prg5.py
@@ -0,0 +1,32 @@
+# testing materials
+import qrcode
+color = ["black","white"]
+def qrGenerator(bd,bx):
+    qr = qrcode.QRCode(
+        version=1,
+        border=bd,
+        box_size=bx,
+        error_correction=qrcode.constants.ERROR_CORRECT_L
+    )
+    return qr
+
+def qrInput():
+    wow = input("Enter the content of qr ~ ")
+    b_size = input("Enter Custom border size ~ ")
+    box_size = input("Enter Custom box size ~ ")
+    c_c = input("need Diffrent Color Combinations ?? y/n ~ ").lower()
+    if c_c == "y":
+        f_c = input("giv fill color ~ ")
+        b_c = input("giv background color ~ ")
+    else:
+        print(f"By default colors :{color[0]}, {color[1]} ")
+        f_c = color[0]
+        b_c = color[1]
+    qr = qrGenerator(b_size,box_size)
+    qr.add_data(wow)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color=f_c,back_color=b_c)
+    img.save("wow_QR.png")
+    print("img saved")
+
+qrInput()
