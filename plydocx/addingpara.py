718d9f020ffb8b36f9fc3b1edbe029ca025de354
Muh Roshan <leoparmigiana@gmail.com>
2023-08-30 13:45:44 +0530

    checked para types

---
+++ plydocx/addingpara.py
@@ -0,0 +1,20 @@
+from docx import Document
+
+d = Document()
+
+d.add_heading("adding para",level=0)
+
+para = d.add_paragraph()
+# bold para
+para.add_run('wow its nice').bold = True
+# italic para on same stanzzza
+para.add_run(' oopz').italic = True
+
+paragraph = d.add_paragraph('Normal text, ')
+run = paragraph.add_run('text with emphasis.' ,'Emphasis')
+j = para.add_run(' check ', 'Strong')
+k = para.add_run('its a Subtle Reference check how i look', 'Subtle Reference')
+# run.style = 'Emphasis'
+
+
+d.save("wow.docx")
