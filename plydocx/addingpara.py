5a462b14fb704cbf2cd3cf893cba0d4a236fd0cc
Muh Roshan <leoparmigiana@gmail.com>
2023-08-30 19:14:19 +0530

    font name --setup

--- plydocx/addingpara.py
+++ plydocx/addingpara.py
@@ -13,7 +13,7 @@ para.add_run(' oopz').italic = True
 paragraph = d.add_paragraph('Normal text, ')
 run = paragraph.add_run('text with emphasis.' ,'Emphasis')
 j = para.add_run(' check ', 'Strong')
-k = para.add_run('its a Subtle Reference check how i look', 'Subtle Reference')
+k = para.add_run('its a Subtle Reference check how i look', 'Subtle Reference').font.name = 'Times New Roman'
 # run.style = 'Emphasis'
 
 
