22b0f4003a07bff76dbf74c80c1c10121fd91414
Muh Roshan <leoparmigiana@gmail.com>
2023-08-30 19:16:50 +0530

    font sizing set

--- plydocx/addingpara.py
+++ plydocx/addingpara.py
@@ -1,4 +1,5 @@
 from docx import Document
+from docx.shared import Pt # for font sizing...
 
 d = Document()
 
@@ -13,7 +14,10 @@ para.add_run(' oopz').italic = True
 paragraph = d.add_paragraph('Normal text, ')
 run = paragraph.add_run('text with emphasis.' ,'Emphasis')
 j = para.add_run(' check ', 'Strong')
-k = para.add_run('its a Subtle Reference check how i look', 'Subtle Reference').font.name = 'Times New Roman'
+# added font name
+k = para.add_run('its a Subtle Reference check how i look', 'Subtle Reference')
+k.font.name = 'Times New Roman'
+k.font.size = Pt(25)
 # run.style = 'Emphasis'
 
 
