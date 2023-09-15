2b5672526af31b10a381ba48b2f1748aeec22a5e
Muh Roshan <leoparmigiana@gmail.com>
2023-09-15 20:35:02 +0530

    almost there...

--- plydocx/create.py
+++ plydocx/create.py
@@ -1,7 +1,12 @@
 from  docx import Document
-
+from docx.shared import Inches
 d = Document()
 
-j = d.add_heading('Muhammed Roshan P S',4)
+j = d.add_paragraph('Muhammed Roshan P S')
+j.add_run('owk')
+j.add_run('oo')
+j.style = 'List Bullet'
+# j.paragraph_format.left_indent = Inches(0.03)
+
 
 d.save('wow.docx')
--- plydocx/model1.py
+++ plydocx/model1.py
@@ -6,7 +6,7 @@ from docx.enum.dml import MSO_THEME_COLOR #MSO theme selector color
 from docx.enum.text import WD_ALIGN_PARAGRAPH # alignmennt
 from docx.enum.style import WD_STYLE
 from functions import insertHR,add_hyperlink
-
+from docx.enum.text import WD_LINE_SPACING # line spacing
 
 #add a hyperlink with a custom color and no underline
 # hyperlink = add_hyperlink(p, 'http://www.google.com', 'Google', 'FF8822', False)
@@ -19,20 +19,33 @@ from functions import insertHR,add_hyperlink
 d = Document()
 
 # for para creation simplicity 
-def paara():
+def paara(spacing=False,Bbullet=False):
     para = d.add_paragraph('')
+    if spacing == True:
+        para.paragraph_format.space_before = Pt(1)
+        para.paragraph_format.space_after = Pt(1)
+    if Bbullet == 1:
+        para.style = 'List Bullet'
     return para
 
 # to setup font clr,theme,name for simpliity
 # fff(dname=l,size=int(val),theme ='MSO_THEME_COLOR.ACCENT_1',fntname='Open Sans')
-def fff(dname,size=int(11),theme=MSO_THEME_COLOR.ACCENT_1,fntname='Open Sans',bbold=False):
+def fff(dname,size=int(11),theme=MSO_THEME_COLOR.ACCENT_1,fntname='Open Sans',bbold=False,iitalic=False):
     dname.font.size = Pt(size)
     dname.font.color.theme_color = theme
     if bbold == True:
         dname.bold = True
     else:
         pass
+    if iitalic == 1:
+        dname.italic = True
+    else:
+        pass
     dname.font.name = fntname
+
+def addBlank(dname,count):
+    for _ in range(count):
+        dname.add_text(" ")
 
 
 #####################
@@ -70,7 +83,7 @@ add_hyperlink(nxt, url='https://github.com/Roshan-Here', text='Github |',color='
 add_hyperlink(nxt, url='https://www.linkedin.com/in/muhammed-roshan-ps/', text=' Linkdin |',color='0A66C2',underline=True)
 add_hyperlink(nxt, url='http://mailto:muhammedroshanps@gmail.com', text=' muhammedroshanps@gmail.com |',color='2b3137',underline=True)
 add_hyperlink(nxt, url='http://tel:9809031477', text=' +919809031477',color='5B51D8',underline=True)
-insertHR(nxt,fontsize='20')
+# insertHR(nxt,fontsize='20')
 # add_hyperlink(nxt, text=' | Instagram ', url='http://google.com')
 # add_hyperlink(nxt, text=' | Wiki ', url='http://google.com')
 # k.font.size = Pt(10)
@@ -81,24 +94,84 @@ insertHR(nxt,fontsize='20')
 # Main Body
 ##############################################
 
-# heading-1
+# HEADING-1
 
-para = d.add_heading('',level=3)
+para = d.add_heading('',level=5)
 l = para.add_run('TECHNICAL SKILLS')
 l.font.size = Pt(12)
 l.font.color.theme_color = MSO_THEME_COLOR.ACCENT_1
 l.font.name = 'Calibri'
 insertHR(para,color='black')
 # below the heading....
-para = paara() # mov on to simplicity
-l = para.add_run('   Languages : ')
+para = paara(spacing=True) # mov on to simplicity
+l = para.add_run('   Languages   : ')
 j = para.add_run('Python,C,JavaScript...etc')
+# para.paragraph_format.line_spacing_rule = WD_LINE_SPACING.EXACTLY
 fff(l,size=int(12),theme=MSO_THEME_COLOR.DARK_1,fntname='Calibri',bbold=True)
-para = paara()
-l = para.add_run('   Languages : ')
-j = para.add_run('Python,C,JavaScript...etc')
+para = paara(spacing=True)
+l = para.add_run('   Database     : ')
+j = para.add_run('MySql,MongoDb...etc') # change... later
+# para.paragraph_format.line_spacing = 0.5
+fff(l,size=int(12),theme=MSO_THEME_COLOR.DARK_1,fntname='Calibri',bbold=True)
+para = paara(spacing=True)
+l = para.add_run('   Libraries      : ')
+j = para.add_run('BeautifulSoup,Pyrogram,...etc') # change... later
+fff(l,size=int(12),theme=MSO_THEME_COLOR.DARK_1,fntname='Calibri',bbold=True)
+para = paara(spacing=True)
+l = para.add_run('   Dev Tools    : ')
+j = para.add_run('Visual Studio Code, Pycharm, Github, Vim, OpenAI') # change... later
 fff(l,size=int(12),theme=MSO_THEME_COLOR.DARK_1,fntname='Calibri',bbold=True)
 
+
+# HEADING-2
+
+para = d.add_heading('',level=5)
+l = para.add_run('EDUCATION')
+l.font.size = Pt(12)
+l.font.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+l.font.name = 'Calibri'
+insertHR(para,color='black')
+para = paara(spacing=True)
+l = para.add_run('   SNMIMT COLLEGE OF ENGINEERING')
+addBlank(l,68) # count depends on location string.len 
+j = para.add_run('Kerala,India')
+fff(l,size=int(12),bbold=True,theme=MSO_THEME_COLOR.DARK_1,fntname='Tahoma')
+para = paara(spacing=True)
+l = para.add_run('    Bachelor of Science in Computer Science')
+fff(l,size=int(10),iitalic=True,fntname='Calibri',theme=MSO_THEME_COLOR.DARK_1)
+para = paara()
+l = para.add_run('    Ongoing (Expected Graduation: May 2024)') #ask if its compled/ongoing on form 
+fff(l,size=int(10),iitalic=True,fntname='Calibri',theme=MSO_THEME_COLOR.DARK_1)
+
+
+para = paara(spacing=True)
+l = para.add_run('   Govt Boys Aluva')
+addBlank(l,103)
+j = para.add_run('Kerala,India')
+fff(l,size=int(12),bbold=True,theme=MSO_THEME_COLOR.DARK_1,fntname='Tahoma')
+para = paara(spacing=True)
+l = para.add_run('    XII - Bio Maths - Science Grad.')
+fff(l,size=int(10),iitalic=True,fntname='Calibri',theme=MSO_THEME_COLOR.DARK_1)
+
+
+# HEADING -3
+
+para = d.add_heading('',level=5)
+l = para.add_run('Projects')
+l.font.size = Pt(12)
+l.font.color.theme_color = MSO_THEME_COLOR.ACCENT_1
+l.font.name = 'Calibri'
+insertHR(para,color='black')
+
+para = paara(spacing=True)
+l = para.add_run('   Students Corner')
+fff(l,size=int(12),bbold=True,theme=MSO_THEME_COLOR.DARK_1,fntname='Tahoma')
+para = paara(spacing=True)
+add_hyperlink(para, url='https://github.com/Roshan-Here', text='    StudentsCorner',color='0A66C2',underline=True)
+
+para = paara()
+l = para.add_run('Bullet Checking')
+l.style = 'List Bullet'
 
 # https://www.resume.com/sample/civil-engineer/
 
